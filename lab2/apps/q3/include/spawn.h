2f92eb7437237d56025036c3bd0aed1541377641
Zeeshan Hussain <zeeshanhussain171@gmail.com>
2025-09-24 13:02:16 -0400

    adding semaphores instead of locks with semaphore signaling empty and full status

---
+++ lab2/apps/q3/include/spawn.h
@@ -0,0 +1,25 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+#include "lab2-api.h"
+
+typedef struct missile_code {
+  int numprocs;
+  char really_important_char;
+} missile_code;
+
+#define BUFFER_SIZE 4
+#define MESSAGE "0123456789"
+
+typedef struct mem_buffer {
+  char buffer[BUFFER_SIZE];
+  int start;
+  int end;
+  int count;
+  int chars_produced; // Global counter for total characters produced
+  int chars_consumed; // Global counter for total characters consumed
+  sem_t full;
+  sem_t empty; 
+} mem_buffer;
+
+#define FILENAME_TO_RUN "spawn_me.dlx.obj"
+#endif
