6cc6f19e84ec656b9ebd6749f50250e6024eb9e8
Zeeshan Hussain <zeeshanhussain171@gmail.com>
2025-09-30 12:11:57 -0400

    lab 4 code for testing after part b

---
+++ lab2/apps/q4/include/spawn.h
@@ -0,0 +1,24 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+#include "lab2-api.h"
+
+typedef struct missile_code {
+  int numprocs;
+  char really_important_char;
+} missile_code;
+
+#define BUFFER_SIZE 4
+#define MESSAGE "0123456789"
+
+typedef struct mem_buffer {
+  char buffer[BUFFER_SIZE];
+  int start;
+  int end;
+  cond_t notfull; // condition variable to indicate buffer is not full
+  cond_t notempty; // condition variable to indicate buffer is not empty
+} mem_buffer;
+
+
+#define CONSUMER_TO_RUN "consumer.dlx.obj"
+#define PRODUCER_TO_RUN "producer.dlx.obj"
+#endif
