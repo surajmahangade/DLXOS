86bc6839b818b2b7a1ded86899be78d451cbd847
Suraj Sanjay Mahangade <smahanga@ececomp1.ecn.purdue.edu>
2025-10-30 23:09:33 -0400

    added again

---
+++ lab3copy/one-level/include/os/memory_constants.h
@@ -0,0 +1,24 @@
+#ifndef	_memory_constants_h_
+#define	_memory_constants_h_
+
+//------------------------------------------------
+// #define's that you are given:
+//------------------------------------------------
+
+// We can read this address in I/O space to figure out how much memory
+// is available on the system.
+#define	DLX_MEMSIZE_ADDRESS	0xffff0000
+
+// Return values for success and failure of functions
+#define MEM_SUCCESS 1
+#define MEM_FAIL -1
+
+//--------------------------------------------------------
+// Put your constant definitions related to memory here.
+// Be sure to prepend any constant names with "MEM_" so 
+// that the grader knows they are defined in this file.
+
+//--------------------------------------------------------
+
+
+#endif	// _memory_constants_h_
