ee6fac0b85ed9b49293a0367bcb4de52e1fe2944
Suraj Sanjay Mahangade <smahanga@ececomp1.ecn.purdue.edu>
2025-11-02 11:32:41 -0500

    heap management ode copied form one level

---
+++ lab3copy/heap-mgmt/apps/example/trap_address/trap_address.c
@@ -0,0 +1,33 @@
+#include "usertraps.h"
+#include "misc.h"
+
+void main (int argc, char *argv[])
+{
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+  int *p;
+  int x;
+  if (argc != 2) { 
+    Printf("Usage: %s <handle_to_procs_completed_semaphore>\n"); 
+    Exit();
+  } 
+
+  // Convert the command-line strings into integers for use as handles
+  s_procs_completed = dstrtol(argv[1], NULL, 10);
+
+  // Now print a message to show that everything worked
+  Printf("TRAP_ADDRESS (%d): Hello world! tmo\n", getpid());
+
+  p = (int*)(0x003dFFFD); // in range but not allocated
+  Printf("TRAP_ADDRESS (%d): accessing address %d, size of int %d\n", getpid(), p, sizeof(int));
+  x = *p;        // READ → should raise TRAP_ADDRESS (range)
+  // print x
+  Printf("TRAP_ADDRESS (%d): read value %d from address %d\n", getpid(), x, p);
+
+  // Signal the semaphore to tell the original process that we're done
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("TRAP_ADDRESS (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+    Exit();
+  }
+
+  Printf("TRAP_ADDRESS (%d): Done!\n", getpid());
+}
