720a7df41278160556c51dcd1c5dfc0f194bc1fc
Zeeshan Hussain <zeeshanhussain171@gmail.com>
2025-11-27 22:27:57 -0500

    question 3 addition in the code

--- lab4/flat/os/dfs.c
+++ lab4/flat/os/dfs.c
@@ -17,6 +17,7 @@ static inline uint32 invert(uint32 n) { return n ^ negativeone; }
 // additional locks if it is really necessary.
 static int dfs_open = 0;  // Flag to track if filesystem is open
 static lock_t fbv_lock;   // Lock for free block vector operations
+static lock_t inode_lock;  // global variable related to Q3
 // STUDENT: put your file system level functions below.
 // Some skeletons are provided. You can implement additional functions.
 
@@ -33,6 +34,7 @@ void DfsModuleInit() {
 // You essentially set the file system as invalid and then open 
 // using DfsOpenFileSystem().
   fbv_lock = LockCreate();
+  inode_lock = LockCreate(); // global variable related to Q3
   dfs_open = 0;
   if (DfsOpenFileSystem() == DFS_FAIL) {
     printf("DfsModuleInit: Failed to open filesystem\n");
@@ -344,8 +346,18 @@ int DfsWriteBlock(uint32 blocknum, dfs_block *b) {
 //-----------------------------------------------------------------
 
 uint32 DfsInodeFilenameExists(char *filename) {
+  int i;
+  if (!dfs_open) {
     return DFS_FAIL;
-
+  }
+  
+  for (i = 0; i < sb.num_inodes; i++) {
+    if (inodes[i].inuse && dstrncmp(inodes[i].filename, filename, 44) == 0) {
+      return i;
+    }
+  }
+  
+  return DFS_FAIL;
 }
 
 
@@ -358,8 +370,41 @@ uint32 DfsInodeFilenameExists(char *filename) {
 //-----------------------------------------------------------------
 
 uint32 DfsInodeOpen(char *filename) {
+  int i;
+  uint32 handle;
+  
+  if (!dfs_open) {
     return DFS_FAIL;
-
+  }
+  
+  // Check if file already exists
+  handle = DfsInodeFilenameExists(filename);
+  if (handle != DFS_FAIL) {
+    return handle;
+  }
+  
+  // Allocate new inode
+  if (LockHandleAcquire(inode_lock) != SYNC_SUCCESS) {
+    return DFS_FAIL;
+  }
+  
+  for (i = 0; i < sb.num_inodes; i++) {
+    if (!inodes[i].inuse) {
+      inodes[i].inuse = 1;
+      inodes[i].filesize = 0;
+      dstrncpy(inodes[i].filename, filename, 44);
+      for (int j = 0; j < 10; j++) {
+        inodes[i].direct[j] = 0;
+      }
+      inodes[i].indirect = 0;
+      inodes[i].double_indirect = 0;
+      LockHandleRelease(inode_lock);
+      return i;
+    }
+  }
+  
+  LockHandleRelease(inode_lock);
+  return DFS_FAIL;  // No free inodes
 }
 
 
@@ -372,9 +417,73 @@ uint32 DfsInodeOpen(char *filename) {
 //-----------------------------------------------------------------
 
 int DfsInodeDelete(uint32 handle) {
+  int i;
+  dfs_block blk;
+  uint32 *indirect_table;
+  uint32 *double_indirect_table;
+  uint32 indirect_block;
+  
+  if (!dfs_open || handle >= sb.num_inodes) {
     return DFS_FAIL;
-
+  }
+  
+  if (!inodes[handle].inuse) {
+    return DFS_FAIL;
+  }
+  
+  // Free direct blocks
+  for (i = 0; i < 10; i++) {
+    if (inodes[handle].direct[i] != 0) {
+      DfsFreeBlock(inodes[handle].direct[i]);
+      inodes[handle].direct[i] = 0;
+    }
+  }
+  
+  // Free indirect blocks
+  if (inodes[handle].indirect != 0) {
+    DfsReadBlock(inodes[handle].indirect, &blk);
+    indirect_table = (uint32*)blk.data;
+    for (i = 0; i < (sb.blocksize / sizeof(uint32)); i++) {
+      if (indirect_table[i] != 0) {
+        DfsFreeBlock(indirect_table[i]);
+      }
+    }
+    DfsFreeBlock(inodes[handle].indirect);
+    inodes[handle].indirect = 0;
+  }
+  
+  // Free double indirect blocks
+  if (inodes[handle].double_indirect != 0) {
+    DfsReadBlock(inodes[handle].double_indirect, &blk);
+    double_indirect_table = (uint32*)blk.data;
+    for (i = 0; i < (sb.blocksize / sizeof(uint32)); i++) {
+      if (double_indirect_table[i] != 0) {
+        indirect_block = double_indirect_table[i];
+        DfsReadBlock(indirect_block, &blk);
+        indirect_table = (uint32*)blk.data;
+        for (int j = 0; j < (sb.blocksize / sizeof(uint32)); j++) {
+          if (indirect_table[j] != 0) {
+            DfsFreeBlock(indirect_table[j]);
+          }
+        }
+        DfsFreeBlock(indirect_block);
+      }
+    }
+    DfsFreeBlock(inodes[handle].double_indirect);
+    inodes[handle].double_indirect = 0;
+  }
+  
+  // Mark inode as free
+  if (LockHandleAcquire(inode_lock) != SYNC_SUCCESS) {
+    return DFS_FAIL;
+  }
+  inodes[handle].inuse = 0;
+  inodes[handle].filesize = 0;
+  LockHandleRelease(inode_lock);
+  
+  return DFS_SUCCESS;
 }
+
 
 
 //-----------------------------------------------------------------
@@ -385,8 +494,43 @@ int DfsInodeDelete(uint32 handle) {
 //-----------------------------------------------------------------
 
 int DfsInodeReadBytes(uint32 handle, void *mem, int start_byte, int num_bytes) {
+  int bytes_read = 0;
+  int virtual_block, block_offset;
+  uint32 fs_block;
+  dfs_block blk;
+  int bytes_to_read;
+  
+  if (!dfs_open || handle >= sb.num_inodes || !inodes[handle].inuse) {
     return DFS_FAIL;
-
+  }
+  
+  if (start_byte >= inodes[handle].filesize) {
+    return 0;
+  }
+  
+  if (start_byte + num_bytes > inodes[handle].filesize) {
+    num_bytes = inodes[handle].filesize - start_byte;
+  }
+  
+  while (bytes_read < num_bytes) {
+    virtual_block = (start_byte + bytes_read) / sb.blocksize;
+    block_offset = (start_byte + bytes_read) % sb.blocksize;
+    
+    fs_block = DfsInodeTranslateVirtualToFilesys(handle, virtual_block);
+    if (fs_block == 0 || fs_block == DFS_FAIL) {
+      return DFS_FAIL;
+    }
+    
+    if (DfsReadBlock(fs_block, &blk) == DFS_FAIL) {
+      return DFS_FAIL;
+    }
+    
+    bytes_to_read = min(num_bytes - bytes_read, sb.blocksize - block_offset);
+    bcopy(blk.data + block_offset, (char*)mem + bytes_read, bytes_to_read);
+    bytes_read += bytes_to_read;
+  }
+  
+  return bytes_read;
 }
 
 
@@ -400,9 +544,49 @@ int DfsInodeReadBytes(uint32 handle, void *mem, int start_byte, int num_bytes) {
 //-----------------------------------------------------------------
 
 int DfsInodeWriteBytes(uint32 handle, void *mem, int start_byte, int num_bytes) {
-
+  int bytes_written = 0;
+  int virtual_block, block_offset;
+  uint32 fs_block;
+  dfs_block blk;
+  int bytes_to_write;
+  
+  if (!dfs_open || handle >= sb.num_inodes || !inodes[handle].inuse) {
     return DFS_FAIL;
-
+  }
+  
+  while (bytes_written < num_bytes) {
+    virtual_block = (start_byte + bytes_written) / sb.blocksize;
+    block_offset = (start_byte + bytes_written) % sb.blocksize;
+    
+    fs_block = DfsInodeTranslateVirtualToFilesys(handle, virtual_block);
+    
+    if (fs_block == 0 || fs_block == DFS_FAIL) {
+      fs_block = DfsInodeAllocateVirtualBlock(handle, virtual_block);
+      if (fs_block == DFS_FAIL) {
+        return DFS_FAIL;
+      }
+      bzero(blk.data, sb.blocksize);
+    } else {
+      if (DfsReadBlock(fs_block, &blk) == DFS_FAIL) {
+        return DFS_FAIL;
+      }
+    }
+    
+    bytes_to_write = min(num_bytes - bytes_written, sb.blocksize - block_offset);
+    bcopy((char*)mem + bytes_written, blk.data + block_offset, bytes_to_write);
+    
+    if (DfsWriteBlock(fs_block, &blk) == DFS_FAIL) {
+      return DFS_FAIL;
+    }
+    
+    bytes_written += bytes_to_write;
+  }
+  
+  if (start_byte + num_bytes > inodes[handle].filesize) {
+    inodes[handle].filesize = start_byte + num_bytes;
+  }
+  
+  return bytes_written;
 }
 
 
@@ -413,8 +597,10 @@ int DfsInodeWriteBytes(uint32 handle, void *mem, int start_byte, int num_bytes) 
 //-----------------------------------------------------------------
 
 uint32 DfsInodeFilesize(uint32 handle) {
+  if (!dfs_open || handle >= sb.num_inodes || !inodes[handle].inuse) {
     return DFS_FAIL;
-
+  }
+  return inodes[handle].filesize;
 }
 
 
@@ -429,9 +615,79 @@ uint32 DfsInodeFilesize(uint32 handle) {
 //-----------------------------------------------------------------
 
 uint32 DfsInodeAllocateVirtualBlock(uint32 handle, uint32 virtual_blocknum) {
+  uint32 new_block;
+  dfs_block blk;
+  uint32 *table;
+  uint32 indirect_block;
+  int entries_per_block = sb.blocksize / sizeof(uint32);
+  
+  if (!dfs_open || handle >= sb.num_inodes || !inodes[handle].inuse) {
     return DFS_FAIL;
-
-
+  }
+  
+  new_block = DfsAllocateBlock();
+  if (new_block == DFS_FAIL) {
+    return DFS_FAIL;
+  }
+  
+  // Direct blocks
+  if (virtual_blocknum < 10) {
+    inodes[handle].direct[virtual_blocknum] = new_block;
+    return new_block;
+  }
+  
+  // Single indirect
+  if (virtual_blocknum < 10 + entries_per_block) {
+    if (inodes[handle].indirect == 0) {
+      inodes[handle].indirect = DfsAllocateBlock();
+      if (inodes[handle].indirect == DFS_FAIL) {
+        DfsFreeBlock(new_block);
+        return DFS_FAIL;
+      }
+      bzero(blk.data, sb.blocksize);
+      DfsWriteBlock(inodes[handle].indirect, &blk);
+    }
+    DfsReadBlock(inodes[handle].indirect, &blk);
+    table = (uint32*)blk.data;
+    table[virtual_blocknum - 10] = new_block;
+    DfsWriteBlock(inodes[handle].indirect, &blk);
+    return new_block;
+  }
+  
+  // Double indirect
+  virtual_blocknum -= (10 + entries_per_block);
+  
+  if (inodes[handle].double_indirect == 0) {
+    inodes[handle].double_indirect = DfsAllocateBlock();
+    if (inodes[handle].double_indirect == DFS_FAIL) {
+      DfsFreeBlock(new_block);
+      return DFS_FAIL;
+    }
+    bzero(blk.data, sb.blocksize);
+    DfsWriteBlock(inodes[handle].double_indirect, &blk);
+  }
+  
+  DfsReadBlock(inodes[handle].double_indirect, &blk);
+  table = (uint32*)blk.data;
+  
+  if (table[virtual_blocknum / entries_per_block] == 0) {
+    table[virtual_blocknum / entries_per_block] = DfsAllocateBlock();
+    if (table[virtual_blocknum / entries_per_block] == DFS_FAIL) {
+      DfsFreeBlock(new_block);
+      return DFS_FAIL;
+    }
+    DfsWriteBlock(inodes[handle].double_indirect, &blk);
+    bzero(blk.data, sb.blocksize);
+    DfsWriteBlock(table[virtual_blocknum / entries_per_block], &blk);
+  }
+  
+  indirect_block = table[virtual_blocknum / entries_per_block];
+  DfsReadBlock(indirect_block, &blk);
+  table = (uint32*)blk.data;
+  table[virtual_blocknum % entries_per_block] = new_block;
+  DfsWriteBlock(indirect_block, &blk);
+  
+  return new_block;
 }
 
 
@@ -443,6 +699,45 @@ uint32 DfsInodeAllocateVirtualBlock(uint32 handle, uint32 virtual_blocknum) {
 //-----------------------------------------------------------------
 
 uint32 DfsInodeTranslateVirtualToFilesys(uint32 handle, uint32 virtual_blocknum) {
+  dfs_block blk;
+  uint32 *table;
+  uint32 indirect_block;
+  int entries_per_block = sb.blocksize / sizeof(uint32);
+  
+  if (!dfs_open || handle >= sb.num_inodes || !inodes[handle].inuse) {
     return DFS_FAIL;
-
+  }
+  
+  // Direct blocks (0-9)
+  if (virtual_blocknum < 10) {
+    return inodes[handle].direct[virtual_blocknum];
+  }
+  
+  // Single indirect (10 - 10+entries_per_block-1)
+  if (virtual_blocknum < 10 + entries_per_block) {
+    if (inodes[handle].indirect == 0) {
+      return 0;
+    }
+    DfsReadBlock(inodes[handle].indirect, &blk);
+    table = (uint32*)blk.data;
+    return table[virtual_blocknum - 10];
+  }
+  
+  // Double indirect
+  if (inodes[handle].double_indirect == 0) {
+    return 0;
+  }
+  
+  virtual_blocknum -= (10 + entries_per_block);
+  DfsReadBlock(inodes[handle].double_indirect, &blk);
+  table = (uint32*)blk.data;
+  indirect_block = table[virtual_blocknum / entries_per_block];
+  
+  if (indirect_block == 0) {
+    return 0;
+  }
+  
+  DfsReadBlock(indirect_block, &blk);
+  table = (uint32*)blk.data;
+  return table[virtual_blocknum % entries_per_block];
 }
